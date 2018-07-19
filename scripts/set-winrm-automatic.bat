6c2555bce14f138b216c2b53d2aa7c50882191e4
Dave Dix <david@dixieworld.co.uk>
2018-07-19 20:36:05 +0100

    Fix Vagrant start-up issue
    First boot of vagrant often fails due to a timing window
    with winrm connecting before the VM is ready.
    This fix disables winrm and sets a RunOnce command
    to set it to auto on first boot

--- scripts/set-winrm-automatic.bat
+++ scripts/set-winrm-automatic.bat
@@ -1,3 +1,7 @@
 echo Set WinRM start type to auto at first boot of box
 sc config winrm start= disabled
+<<<<<<< HEAD
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce /v StartWinRM /t REG_SZ /f /d "cmd.exe /c \"sc config winrm start= auto ^& sc start winrm\""
+=======
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce /v StartWinRM /t REG_SZ /f /d "cmd.exe /c \"sc config winrm start= auto ^& sc start winrm\""
+>>>>>>> Fix Vagrant start-up issue
