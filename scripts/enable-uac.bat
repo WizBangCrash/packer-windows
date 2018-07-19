0fc75af68853754f3bbb9b19a6ecd1f244176598
Dave Dix <david@dixieworld.co.uk>
2018-07-19 16:00:54 +0100

    Remove UAC prompt

--- scripts/enable-uac.bat
+++ scripts/enable-uac.bat
@@ -1,1 +1,2 @@
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
